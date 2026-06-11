a6d581ea6d2bad9530789e462e141c554cfc4800
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2026-06-11 19:43:03 +0300

    jwt in cookies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const log = console.log;
 const PORT = process.env.PORT || 5500;
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import multer from "multer";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 // import cors from "cors";
+import { jwtInCookies } from "./utilities/gentoken.cookies.js";
 import colors from "colors";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
@@ -17,11 +21,13 @@ import userRouter from "./routers/userRouter.js";
 import messageRouter from "./routers/messageRoutes.js";
 // Middlewares
 import { errorHandling } from "./middlewares/errorMiddleware.js";
+import { doYouHaveCookie } from "./middlewares/haveCookie.js";
 import { mock } from "./middlewares/mockup.js";
 
 app.use(mock);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(morgan("dev"));
 // ATTENTION FRONTEND PORT
 // app.use(
@@ -34,6 +40,7 @@ app.use(morgan("dev"));
 // ROUTES URL ---
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/message", messageRouter);
+app.use(doYouHaveCookie);
 app.use(errorHandling);
 
 // TRY
@@ -44,6 +51,35 @@ app.post("/try", data.single("cv"), (req, res) => {
   res.json({
     a: req.file,
     b: req.body,
+  });
+});
+
+// console.log(path.resolve("controllers"));
+
+// log("import.meta.url".bold.bgGreen, import.meta.url);
+// log(
+//   "fileURLToPath(import.meta.url)".bold.bgGreen,
+//   fileURLToPath(import.meta.url),
+// );
+// log("__DIRNAME".bold.bgYellow, path.resolve());
+// log("__FILENAME".bold.bgYellow, path.dirname(path.resolve()));
+// log("process.cwd()".bold.bgGreen, process.cwd());
+
+// PRODUCTION MOOOD
+if (process.env.ENVIROMENT === "production") {
+  // الأول لتقديم الملفات الثابتة (JS, CSS, Images)، والثاني ليجعل جميع مسارات React تعمل حتى بعد إعادة تحميل الصفحة.
+  app.use(express.static(path.join(path.resolve(), "../dist/index.html")));
+
+  app.get("*", (req, res) => {
+    app.sendFile(path.join(path.resolve(), "../dist/index.html"));
+  });
+}
+
+app.get("/help", async (req, res) => {
+  const name = " req.body.needHelp";
+  // await jwtInCookies(id, res);
+  res.status(200).json({
+    name,
   });
 });
 

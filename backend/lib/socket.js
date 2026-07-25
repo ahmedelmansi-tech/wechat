a7d6105624bc7d24583f5bb172d078fe54feddbf
ahmedelmansi-tech <ahmed.helmii911@gmail.com>
2026-07-25 18:15:12 +0300

    socket server - socket client online users

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -2,17 +2,42 @@ import { createServer } from "node:http";
 import express from "express";
 import { Server } from "socket.io";
 
-import { socketAuthMiddleWare } from "../middlewares/socket.auth";
+import { socketAuthMiddleWare } from "../middlewares/socket.auth.js";
 
 const app = express();
 const server = createServer(app);
-
+console.log(`hello from SOCKET :${process.env.VITE_WECHAT_URL}`.bgYellow);
 // Socket Server
 const io = new Server(server, {
   cors: {
-    origin: [process.env.VITE_WECHAT_URL],
+    origin: ["http://localhost:5173"],
     credentials: true,
   },
 });
 
 io.use(socketAuthMiddleWare);
+io.use((socket, next) => {
+  console.log("Socket Reached");
+  next();
+});
+
+const onLineUsers = {};
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket?.user.name} Connected`.bgMagenta);
+  console.log(`User Id ${socket?.userId} Connected`.bgMagenta);
+
+  const socketId = socket?.userId;
+  onLineUsers[socketId] = socket.userId;
+
+  io.emit("onlineUsers", Object.keys(onLineUsers));
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket?.user.name} disconnected`.bgMagenta);
+
+    delete onLineUsers[socketId];
+    io.emit("onlineUsers", Object.keys(onLineUsers));
+  });
+});
+
+export { server, app, io };
